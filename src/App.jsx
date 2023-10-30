84a5b01df13cbb7d828370ba973ccfe0a99b6546
Ryan Pham <ryanphamit@gmail.com>
2023-10-30 21:22:53 +0700

    add MUI theme & prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
 import { Button } from '@mui/material'
+import Typography from '@mui/material/Typography'
 
 function App() {
-
   return (
     <>
+      <Typography variant='body2' color='text.secondary'>
+        Test Typography
+      </Typography>
       <Button variant='contained'>Hello</Button>
     </>
-  );
+  )
 }
 
 export default App
