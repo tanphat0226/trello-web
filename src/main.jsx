84fcc3af7d478566c7fc7a4f2752fcc8f5d6d205
ryanpham <ryanphamit@gmail.com>
2023-11-17 20:41:08 +0700

    Drag Drop Card#2.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -6,10 +6,10 @@ import App from '~/App.jsx'
 import theme from '~/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </CssVarsProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </CssVarsProvider>
+  // </React.StrictMode>
 )
